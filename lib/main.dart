5f47c9f89e941558cba472c38259d3ac7d50f301
alffrondez <126374472+alffrondez@users.noreply.github.com>
2023-11-24 17:47:16 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:accidentapp/views/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'authlayout.dart';
+import 'firebase_options.dart';
+import 'viewmodels/userViewModel.dart';
+import 'views/home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: StreamBuilder(
+          stream: FirebaseAuth.instance.userChanges(),
+          initialData: null,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return AuthenticatedLayout();
+            }
+            return Login();
+          },
+        ),
+      ),
+    );
+  }
+}
